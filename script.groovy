be62cbe13637065943ab3e299b34bd7f93027244
MoYVR <78653180+MoYVR@users.noreply.github.com>
2023-04-11 15:59:22 -0700

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -5,10 +5,10 @@ def buildJar() {
 
 def buildImage() {
     echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId:'docker-hub-repo', usernameVariable: 'USER', passwordVariable: 'PASS')]) {
-    sh 'docker build -t moyvr/my-repo:jma-3.0 .'
-    sh "echo $PASS | docker login -u $USER --password-stdin"
-    sh 'docker push moyvr/my-repo:jma-3.0'
+    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        sh 'docker build -t moyvr/demo-app:jma-2.0 .'
+        sh "echo $PASS | docker login -u $USER --password-stdin"
+        sh 'docker push moyvr/demo-app:jma-2.0'
     }
 } 
 
