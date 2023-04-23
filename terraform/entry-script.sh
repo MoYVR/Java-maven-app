b9510b86c02afdba1e5f14ed40031952fd62c1d2
MoYVR <78653180+MoYVR@users.noreply.github.com>
2023-04-23 16:57:01 -0700

    updating the docker-compose config

--- terraform/entry-script.sh
+++ terraform/entry-script.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-sudo yum update -y && sudo yum install docker -y
-sudo systemctl start docker
+sudo yum update -y && sudo yum install -y docker
+sudo systemctl start docker 
 sudo usermod -aG docker ec2-user
-docker run -p 8080:80 nginx
 
-# install docker-compose
+# install docker-compose 
 sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
