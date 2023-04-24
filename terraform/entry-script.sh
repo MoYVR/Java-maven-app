67af534169e25bd861e43d89aece7282deb2d123
MoYVR <78653180+MoYVR@users.noreply.github.com>
2023-04-23 18:02:04 -0700

    Update entry-script.sh

--- terraform/entry-script.sh
+++ terraform/entry-script.sh
@@ -4,5 +4,5 @@ sudo systemctl start docker
 sudo usermod -aG docker ec2-user
 
 # install docker-compose 
-sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/2.17.3/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
